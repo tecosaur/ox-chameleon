e51b9197c78d0079b931b9f9492c0840bdb6e90d
TEC <tec@tecosaur.com>
2022-05-04 18:33:06 +0800

    Support custom engrave faces presets
    along with a few other tweaks and fixes

--- ox-chameleon.el
+++ ox-chameleon.el
@@ -23,8 +23,14 @@
   (require 'cl-lib)
   (require 'rx))
 
+(defvar engrave-faces-preset-styles)
+
 (defvar ox-chameleon-snap-fgbg-to-bw nil
   "When non-nil, snap bg/fg colours to black/white when they're close.")
+
+(defvar ox-chameleon-engrave-preset nil
+  "An engrave-faces preset to use when generating stylings.
+When set to nil, the current theme will be used.")
 
 (defvar ox-chameleon--p nil
   "Used to indicate whether the current export is trying to blend in. Set just before being accessed.")
@@ -46,37 +52,51 @@
     (funcall orig-fn info template snippet?)))
 (advice-add 'org-latex-make-preamble :around #'ox-chameleon-org-latex-export)
 
-(defun ox-chameleon-engrave-wrapper (orig-fun backend &rest args)
-  ;; Require modes that provide faces used in `engrave-faces-generate-preset'.
+;; Require modes that provide faces used in `engrave-faces-generate-preset'.
+(unless (interactive-p)
   (require 'highlight-numbers nil t)
   (require 'highlight-quoted nil t)
-  (require 'rainbow-delimiters nil t)
-  (if (and ox-chameleon--p (org-export-derived-backend-p backend 'latex))
-      ;; For some reason a `let' block doesn't seem to work here.
-      (unwind-protect
-          (progn (setq ox-chameleon--ef-styles--old engrave-faces-preset-styles
-                       engrave-faces-preset-styles (engrave-faces-generate-preset))
-                 (apply orig-fun backend args))
-        (setq engrave-faces-preset-styles ox-chameleon--ef-styles--old)
-        (makunbound 'ox-chameleon--ef-styles--old))
-    (apply orig-fun backend args)))
-(advice-add 'org-export-as :around #'ox-chameleon-engrave-wrapper)
+  (require 'rainbow-delimiters nil t))
+
+(defun ox-chameleon-engrave-wrapper (orig-fun contents info)
+  "Generate an engrave preset and bind it to `engrave-faces-preset-styles'.
+This works with `org-latex-template'-style functions."
+  (if (not ox-chameleon--p)
+      (funcall orig-fun contents info)
+    (require 'engrave-faces-latex)
+    (let ((engrave-faces-preset-styles (or ox-chameleon-engrave-preset
+                                           (engrave-faces-generate-preset))))
+      (funcall orig-fun contents info))))
+
+(advice-add 'org-latex-template :around #'ox-chameleon-engrave-wrapper)
+(advice-add 'org-beamer-template :around #'ox-chameleon-engrave-wrapper)
 
 (defun ox-chameleon-generate-colourings (info)
-  (concat
-   "\n%% make document follow Emacs theme\n"
-   (apply #'format "\n\\definecolor{obg}{HTML}{%s}\n\\definecolor{ofg}{HTML}{%s}\n"
-          (ox-chameleon--generate-fgbg-colours))
-   (ox-chameleon--generate-heading-colourings)
-   (ox-chameleon--generate-text-colourings)
-   (if (plist-get info :beamer-theme)
-       (if (string-match-p "default$" (plist-get info :beamer-theme))
-           (ox-chameleon--generate-beamer-colourings)
-         (ox-chameleon--generate-beamer-themed-colourings))
-     (concat "\n\\pagecolor{obg}\n\\color{ofg}\n"
-             (ox-chameleon--generate-koma-structural-colourings)))
-   (ox-chameleon--generate-src-colourings)
-   "\n%% end customisations\n\n"))
+  (let ((engrave-faces-preset-styles (or ox-chameleon-engrave-preset
+                                         (engrave-faces-generate-preset))))
+    (concat
+     "\n%% make document follow Emacs theme\n"
+     (apply #'format "\n\\definecolor{obg}{HTML}{%s}\n\\definecolor{ofg}{HTML}{%s}\n"
+            (ox-chameleon--generate-fgbg-colours))
+     (ox-chameleon--generate-heading-colourings)
+     (ox-chameleon--generate-text-colourings)
+     (if (plist-get info :beamer-theme)
+         (if (string-match-p "default$" (plist-get info :beamer-theme))
+             (ox-chameleon--generate-beamer-colourings)
+           (ox-chameleon--generate-beamer-themed-colourings))
+       (concat "\n\\pagecolor{obg}\n\\color{ofg}\n"
+               (ox-chameleon--generate-koma-structural-colourings)))
+     (ox-chameleon--generate-src-colourings)
+     "\n%% end customisations\n\n")))
+
+(defun ox-chameleon--face-attr (face attr)
+  (if-let ((spec (cdr (assoc face engrave-faces-preset-styles)))
+           (value (plist-get spec attr)))
+      value
+    (when engrave-faces-preset-styles
+      (message "ox-chameleon: %s %s not provided, falling back to current theme."
+               face attr))
+    (face-attribute face attr nil 'default)))
 
 (defun ox-chameleon--hex-to-srgb (hex)
   (mapcar (lambda (range) (/ (string-to-number (apply #'substring hex range) 16) 255.0))
@@ -84,15 +104,15 @@
 
 (defun ox-chameleon--generate-fgbg-colours ()
   (mapcar (lambda (hex) (substring hex 1))
-          (let ((bg (face-attribute 'default :background))
-                (fg (face-attribute 'default :foreground)))
+          (let ((bg (ox-chameleon--face-attr 'default :background))
+                (fg (ox-chameleon--face-attr 'default :foreground)))
             (if ox-chameleon-snap-fgbg-to-bw
-                (list bg fg)
-              (cl-destructuring-bind ((hb sb lb) (hf sf lf))
-                  (list (apply #'color-rgb-to-hsl (ox-chameleon--hex-to-srgb bg))
-                        (apply #'color-rgb-to-hsl (ox-chameleon--hex-to-srgb fg)))
-                (list (if (and (> lb 0.95) (< (* sb (- 1 lb)) 0.01)) "#ffffff" bg)
-                      (if (and (< lf 0.4) (< (* sf lf) 0.1)) "#000000" fg)))))))
+                (cl-destructuring-bind ((hb sb lb) (hf sf lf))
+                    (list (apply #'color-rgb-to-hsl (ox-chameleon--hex-to-srgb bg))
+                          (apply #'color-rgb-to-hsl (ox-chameleon--hex-to-srgb fg)))
+                  (list (if (and (> lb 0.95) (< (* sb (- 1 lb)) 0.01)) "#ffffff" bg)
+                        (if (and (< lf 0.4) (< (* sf lf) 0.1)) "#000000" fg)))
+              (list bg fg)))))
 
 (defun ox-chameleon--generate-text-colourings ()
   (apply #'format
@@ -117,11 +137,11 @@
 "
          (mapcar (lambda (hex) (substring hex 1))
                  (list
-                  (face-attribute 'link :foreground nil 'default)
-                  (face-attribute 'org-cite :foreground nil 'default)
-                  (face-attribute 'org-list-dt :foreground nil 'default)
-                  (face-attribute 'org-code :foreground nil 'default)
-                  (face-attribute 'org-verbatim :foreground nil 'default)))))
+                  (ox-chameleon--face-attr 'org-link :foreground)
+                  (ox-chameleon--face-attr 'org-cite :foreground)
+                  (ox-chameleon--face-attr 'org-list-dt :foreground)
+                  (ox-chameleon--face-attr 'org-code :foreground)
+                  (ox-chameleon--face-attr 'org-verbatim :foreground)))))
 
 (defun ox-chameleon--generate-src-colourings ()
   (apply #'format
@@ -133,24 +153,12 @@
 \\definecolor{codeborder}{HTML}{%s}
 "
          (mapcar (lambda (hex) (substring hex 1))
-                 (cond
-                  ((and (featurep 'doom-themes) (featurep 'solaire-mode))
-                   (list
-                    (face-attribute 'solaire-default-face :background nil 'default)
-                    (doom-blend (face-attribute 'solaire-default-face :background nil 'default)
-                                (face-attribute 'default :foreground nil 'default)
-                                0.95)))
-                  ((featurep 'doom-themes)
-                   (list
-                    (doom-blend (face-attribute 'default :background nil 'default)
-                                (face-attribute 'default :foreground nil 'default)
-                                0.98)
-                    (doom-blend (face-attribute 'default :background nil 'default)
-                                (face-attribute 'default :foreground nil 'default)
-                                0.95)))
-                  (t (list
-                      (face-attribute 'default :background nil 'default)
-                      (face-attribute 'shadow :foreground nil 'default)))))))
+                 (list (ox-chameleon--face-attr 'org-block :background)
+                       (if (featurep 'doom-themes)
+                           (doom-blend (ox-chameleon--face-attr 'org-block :background)
+                                       (ox-chameleon--face-attr 'default :foreground)
+                                       0.95)
+                         (ox-chameleon--face-attr 'shadow :foreground))))))
 
 (defun ox-chameleon--generate-heading-colourings ()
   (apply #'format
@@ -169,25 +177,25 @@
 "
          (mapcar (lambda (hex) (substring hex 1))
                  (list
-                  (face-attribute 'org-document-title :foreground nil 'default)
-                  (face-attribute 'org-document-info :foreground nil 'default)
-                  (face-attribute 'outline-1 :foreground nil 'default)
-                  (face-attribute 'outline-2 :foreground nil 'default)
-                  (face-attribute 'outline-3 :foreground nil 'default)
-                  (face-attribute 'outline-4 :foreground nil 'default)
-                  (face-attribute 'outline-5 :foreground nil 'default)
-                  (face-attribute 'outline-6 :foreground nil 'default)
-                  (face-attribute 'outline-7 :foreground nil 'default)
-                  (face-attribute 'outline-8 :foreground nil 'default)))))
+                  (ox-chameleon--face-attr 'org-document-title :foreground)
+                  (ox-chameleon--face-attr 'org-document-info :foreground)
+                  (ox-chameleon--face-attr 'outline-1 :foreground)
+                  (ox-chameleon--face-attr 'outline-2 :foreground)
+                  (ox-chameleon--face-attr 'outline-3 :foreground)
+                  (ox-chameleon--face-attr 'outline-4 :foreground)
+                  (ox-chameleon--face-attr 'outline-5 :foreground)
+                  (ox-chameleon--face-attr 'outline-6 :foreground)
+                  (ox-chameleon--face-attr 'outline-7 :foreground)
+                  (ox-chameleon--face-attr 'outline-8 :foreground)))))
 
 (defun ox-chameleon--generate-beamer-structural-colourings ()
   (format
-         "
+   "
 "))
 
 (defun ox-chameleon--generate-koma-structural-colourings ()
   (format
-         "
+   "
 %% structural elements
 
 \\addtokomafont{title}{\\color{documentTitle}}
@@ -216,24 +224,30 @@
 \\renewcommand{\\labelenumiii}{\\textcolor{itemlabel}{\\theenumiii.}}
 \\renewcommand{\\labelenumiv}{\\textcolor{itemlabel}{\\theenumiv.}}
 "
-         (substring (face-attribute 'org-list-dt :foreground nil 'default) 1)))
+   (substring (ox-chameleon--face-attr 'org-list-dt :foreground) 1)))
 
 (defun ox-chameleon--generate-beamer-themed-colourings ()
+  (let ((builtin-fg (substring (ox-chameleon--face-attr 'font-lock-builtin-face :foreground) 1))
+        (varname-fg (substring (ox-chameleon--face-attr 'font-lock-variable-name-face :foreground) 1))
+        (link-fg (substring (ox-chameleon--face-attr 'org-link :foreground) 1))
+        (ol-3-fg (substring (ox-chameleon--face-attr 'outline-3 :foreground) 1))
+        (default-fg (substring (ox-chameleon--face-attr 'default :foreground) 1)))
   (format
    "
 %% beamer
 
 \\definecolor{builtin}{HTML}{%s}
+\\definecolor{varname}{HTML}{%s}
 
 \\NewCommandCopy{\\oldusetheme}{\\usetheme}
 \\renewcommand*{\\usetheme}[2][]{\\oldusetheme[#1]{#2}
-  \\setbeamercolor{progress bar}{fg=builtin}
-  \\setbeamercolor{title separator}{fg=builtin}
-  \\setbeamercolor{progress bar in head/foot}{fg=builtin}
-  \\setbeamercolor{progress bar in section page}{fg=builtin}
+  \\setbeamercolor{title separator}{fg=documentTitle}
+  \\setbeamercolor{progress bar}{fg=documentTitle}
+  \\setbeamercolor{progress bar in head/foot}{fg=documentTitle}
+  \\setbeamercolor{progress bar in section page}{fg=documentTitle}
 
   \\setbeamercolor{normal text}{fg=ofg, bg=obg}
-  \\setbeamercolor{alerted text}{fg=builtin}
+  \\setbeamercolor{alerted text}{fg=%s}
   \\setbeamercolor*{item}{fg=itemlabel}
 %s
 }
@@ -245,15 +259,23 @@
   \\edef\\insertsectionhead{\\begingroup\\noexpand\\hypersetup{hidelinks}\\noexpand\\hyperlink{Navigation\\the\\c@page}{\\unexpanded{#1}}\\endgroup}}
 \\makeatother
 "
-   (substring (face-attribute 'font-lock-builtin-face :foreground nil 'default) 1)
+   builtin-fg
+   varname-fg
+   (if (or (string= builtin-fg ol-3-fg)
+           (string= builtin-fg link-fg)
+           (string= builtin-fg default-fg))
+       "varname" "builtin")
    (pcase org-beamer-theme
      ((rx "metropolis" line-end)
-      "  \\setbeamercolor{block title}{fg=level3, bg=}")
+      (format
+       "  \\setbeamercolor{block title}{fg=%s, bg=}"
+       (if (string= builtin-fg ol-3-fg)
+           "level1" "level3")))
      (_ "  \\setbeamercolor{title}{fg=documentTitle, bg=obg}
   \\setbeamercolor{titlelike}{fg=ofg, bg=obg}
   \\setbeamercolor{section title}{fg=level1, bg=obg}
   \\setbeamercolor{frametitle}{fg=level2, bg=ofg!15!obg}
-  \\setbeamercolor{block title}{fg=level3, bg=}"))))
+  \\setbeamercolor{block title}{fg=level3, bg=}")))))
 
 (defun ox-chameleon--generate-beamer-colourings ()
   (format
@@ -272,12 +294,15 @@
 \\setbeamercolor{progress bar}{fg=builtin}
 
 \\setbeamercolor{normal text}{fg=ofg, bg=obg}
-\\setbeamercolor{alerted text}{fg=builtin}
+\\setbeamercolor{alerted text}{fg=builtin}%s
 \\setbeamercolor*{item}{fg=itemlabel}
 
 \\setbeamercolor{navigation symbols}{fg=ofg!50!obg, bg=ofg!30!obg}
 "
-   (substring (face-attribute 'font-lock-builtin-face :foreground nil 'default) 1)))
+   (substring (ox-chameleon--face-attr 'font-lock-builtin-face :foreground) 1)
+   (if (eq 'italic (ox-chameleon--face-attr 'font-lock-builtin-face :slant))
+       "\n\\setbeamerfont{alerted text}{series=\\bfseries}"
+     "")))
 
 (provide 'ox-chameleon)
 ;;; ox-chameleon.el ends here
